98d11a4ca942dc9a75659b66790ec470a4dddddd
apamaio <104110720+apamaio@users.noreply.github.com>
2026-04-23 01:47:53 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Mirele, você é ESPECIAL e importante demais na minha vida. </h1>
+    return <h1> Mirele, você é muito ESPECIAL, e importante demais na minha vida. 🌞 </h1>
 }
 
 export default Home ;
