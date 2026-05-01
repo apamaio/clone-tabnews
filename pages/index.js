5e4da008cef6693cf2153c17fa39f0e6097abfe3
apamaio <104110720+apamaio@users.noreply.github.com>
2026-05-01 18:30:28 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
 function Home() {
-    return <h1> Mirele, você é muito ESPECIAL, e importante demais na minha vida. 🌞 </h1>
+  return (
+    <h1>
+      {" "}
+      Mirele, você é muito ESPECIAL, e importante demais na minha vida. 🌞{" "}
+    </h1>
+  );
 }
 
-export default Home ;
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
+}
+
+export default Home;
